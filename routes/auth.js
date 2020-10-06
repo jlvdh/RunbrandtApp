f34f25d9da38f0893dec5a3c306fd0f963b2a7ab
Jorg van der Ham <jvdham@gmail.com>
2020-10-06 19:00:26 +0200

    fixed capitalisation

--- routes/auth.js
+++ routes/auth.js
@@ -1,7 +1,7 @@
 const express = require('express')
 const mongoose = require('mongoose')
 const router = express.Router()
-const User = require('../models/user')
+const User = require('../models/User')
 const bcrypt = require('bcrypt')
 
 router.get('/signup', (req, res, next) => {
